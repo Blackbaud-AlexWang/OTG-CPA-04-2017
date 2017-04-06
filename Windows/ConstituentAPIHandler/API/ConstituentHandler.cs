51df3d5e1888182e2f9a66b9a7875e1069261413
Doug Scott <Doug.Scott@blackbaud.me>
2017-04-06 13:19:24 -0400

    moved mappers off to their own class, added giving history endpoint

--- Windows/ConstituentAPIHandler/API/ConstituentHandler.cs
+++ Windows/ConstituentAPIHandler/API/ConstituentHandler.cs
@@ -1,13 +1,9 @@
 ﻿using System;
 using System.Net;
-using System.Net.Http.Headers;
-using System.Text;
 using System.Net.Http;
 using System.Threading.Tasks;
-using System.Web;
 using ConstituentAPIHandler.API;
 using ConstituentAPIHandler.Contracts;
-using Newtonsoft.Json.Linq;
 
 namespace CSHttpClientSample
 {
@@ -15,6 +11,7 @@ namespace CSHttpClientSample
     {
         private static HttpClient _client;
         private static Headers headers;
+        private static ContractMappers _mapper;
 
         public ConstituentHandler(HttpClient client)
         {
@@ -22,6 +19,7 @@ namespace CSHttpClientSample
             _client.DefaultRequestHeaders.Add("bb-api-subscription-key", headers.SubscriptionKey);
             _client.DefaultRequestHeaders.Add("Authorization", $"Bearer {headers.AccessKey}");
             headers = new Headers();
+            _mapper = new ContractMappers();
         }
 
         public async Task<Constituent> GetConstituent(int constituentId)
@@ -30,7 +28,15 @@ namespace CSHttpClientSample
             var response = await _client.GetAsync(uri);
             if (response.StatusCode == HttpStatusCode.Unauthorized) { throw new Exception("Unauthorized, submit new access key"); }
             var result = response.Content;
-            return MapToConstituent(result);
+            return _mapper.MapToConstituent(result);
+        }
+        public async Task<GivingHistory> GetGivingHistory(int constituentId)
+        {
+            var uri = $"https://api.sky.blackbaud.com/constituent/v1/constituents/{constituentId}/givingsummary/lifetimegiving";
+            var response = await _client.GetAsync(uri);
+            if (response.StatusCode == HttpStatusCode.Unauthorized) { throw new Exception("Unauthorized, submit new access key"); }
+            var result = response.Content;
+            return _mapper.MapToGivingHistory(result);
         }
 
         public async Task<int> SearchConstituent(string name)
@@ -40,35 +46,8 @@ namespace CSHttpClientSample
             var response = await _client.GetAsync(uri);
             if (response.StatusCode == HttpStatusCode.Unauthorized) {  throw new Exception("Unauthorized, submit new access key");}
             var result = response.Content;
-            return GetConstitID(result);
+            return _mapper.GetConstitID(result);
         }
 
-        private static Constituent MapToConstituent(HttpContent content)
-        {
-            var responseDate = content.ReadAsStringAsync().Result;
-            var serializedResponse = JObject.Parse(responseDate);
-            return new Constituent
-            {
-                ConstituentId = Convert.ToInt32(serializedResponse["content"]["id"].ToString()),
-                PreferredName = serializedResponse["content"]["preferred_name"].ToString(),
-                LastName = serializedResponse["content"]["last_name"].ToString(),
-                Name = SelectName(serializedResponse),
-                Age = Convert.ToInt32(serializedResponse["content"]["age"].ToString()),
-                EmailAddress = serializedResponse["content"]["email"]["address"].ToString(),
-                WebSite = serializedResponse["content"]["online_presence"]["address"].ToString(),
-                DateAdded = Convert.ToDateTime(serializedResponse["content"]["date_added"].ToString())
-            };
-        }
-        private static int GetConstitID(HttpContent content)
-        {
-            var responseDate = content.ReadAsStringAsync().Result;
-            var serializedResponse = JObject.Parse(responseDate);
-            return Convert.ToInt32(serializedResponse["content"]["value"].First["id"].ToString());
-        }
-
-        private static string SelectName(JObject content)
-        {
-            return content["content"]["preferred_name"] + " " + content["content"]["last_name"];
-        }
     }
 }

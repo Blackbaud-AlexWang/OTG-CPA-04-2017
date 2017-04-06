d66d4fd62cf28b29cceee80d5b831a675312b188
Doug Scott <Doug.Scott@blackbaud.me>
2017-04-06 15:49:52 -0400

    Update AccessKey and altered contract mappers to actually map

--- Windows/ConstituentAPIHandler/Contracts/ContractMappers.cs
+++ Windows/ConstituentAPIHandler/Contracts/ContractMappers.cs
@@ -16,26 +16,26 @@ namespace ConstituentAPIHandler.Contracts
             var serializedResponse = JObject.Parse(responseDate);
             return new Constituent
             {
-                ConstituentId = Convert.ToInt32(serializedResponse["content"]["id"].ToString()),
-                FirstName = serializedResponse["content"]["first"].ToString(),
-                LastName = serializedResponse["content"]["last"].ToString(),
+                ConstituentId = Convert.ToInt32(serializedResponse["id"].ToString()),
+                FirstName = serializedResponse["first"].ToString(),
+                LastName = serializedResponse["last"].ToString(),
                 Name = SelectName(serializedResponse),
-                Age = Convert.ToInt32(serializedResponse["content"]["age"].ToString()),
-                EmailAddress = serializedResponse["content"]["email"]["address"].ToString(),
-                WebSite = serializedResponse["content"]["online_presence"]["address"].ToString(),
-                DateAdded = Convert.ToDateTime(serializedResponse["content"]["date_added"].ToString())
+                Age = Convert.ToInt32(serializedResponse["age"].ToString()),
+                EmailAddress = serializedResponse["email"]["address"].ToString(),
+                WebSite = serializedResponse["online_presence"]["address"].ToString(),
+                DateAdded = Convert.ToDateTime(serializedResponse["date_added"].ToString())
             };
         }
         public int GetConstitID(HttpContent content)
         {
             var responseDate = content.ReadAsStringAsync().Result;
             var serializedResponse = JObject.Parse(responseDate);
-            return Convert.ToInt32(serializedResponse["content"]["value"].First["id"].ToString());
+            return Convert.ToInt32(serializedResponse["value"].First["id"].ToString());
         }
 
         private static string SelectName(JObject content)
         {
-            return content["content"]["first"] + " " + content["content"]["last"];
+            return content["first"] + " " + content["last"];
         }
 
         public GivingHistory MapToGivingHistory(HttpContent content)
@@ -44,14 +44,14 @@ namespace ConstituentAPIHandler.Contracts
             var serializedResponse = JObject.Parse(responseDate);
             return new GivingHistory
             {
-                TotalGiving = Convert.ToDecimal(serializedResponse["content"]["total_giving"]["value"]),
-                TotalReceivedGiving = Convert.ToDecimal(serializedResponse["content"]["total_received_giving"]["value"]),
-                TotalPledgeBalance = Convert.ToDecimal(serializedResponse["content"]["total_pledge_balance"]["value"]),
-                TotalSoftCredits = Convert.ToDecimal(serializedResponse["content"]["total_soft_credits"]["value"]),
-                TotalYearsGiven = Convert.ToInt32(serializedResponse["content"]["total_years_given"]),
-                ConsecutiveYearsGiven = Convert.ToInt32(serializedResponse["content"]["consecutive_years_given"]),
-                TotalCommittedMatchinGifts = Convert.ToInt32(serializedResponse["content"]["total_committed_matching_gifts"]["value"]),
-                TotalReceivedMatchingGifts = Convert.ToInt32(serializedResponse["content"]["total_received_matching_gifts"]["value"]),
+                TotalGiving = Convert.ToDecimal(serializedResponse["total_giving"]["value"]),
+                TotalReceivedGiving = Convert.ToDecimal(serializedResponse["total_received_giving"]["value"]),
+                TotalPledgeBalance = Convert.ToDecimal(serializedResponse["total_pledge_balance"]["value"]),
+                TotalSoftCredits = Convert.ToDecimal(serializedResponse["total_soft_credits"]["value"]),
+                TotalYearsGiven = Convert.ToInt32(serializedResponse["total_years_given"]),
+                ConsecutiveYearsGiven = Convert.ToInt32(serializedResponse["consecutive_years_given"]),
+                TotalCommittedMatchinGifts = Convert.ToInt32(serializedResponse["total_committed_matching_gifts"]["value"]),
+                TotalReceivedMatchingGifts = Convert.ToInt32(serializedResponse["total_received_matching_gifts"]["value"]),
             };
         }
     }

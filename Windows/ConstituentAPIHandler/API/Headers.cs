78b84b3a8e0f68078468c8420168b91834e4d654
Doug Scott <Doug.Scott@blackbaud.me>
2017-04-06 19:21:31 -0400

    Added auth in thie most awesome fashion

--- Windows/ConstituentAPIHandler/API/Headers.cs
+++ Windows/ConstituentAPIHandler/API/Headers.cs
@@ -5,13 +5,12 @@ using System.Web;
 
 namespace ConstituentAPIHandler.API
 {
-    public static class Headers
+    public class Headers
     {
-        public static string SubscriptionKey = "e87327787bbd4c719d6c95ba44a0d0a4";
+        public string SubscriptionKey = "e87327787bbd4c719d6c95ba44a0d0a4";
 
         //access key comes from sky api developer portal. it lasts one hour. don't know better way to get it
-        //3:45 pm
         public static string AccessKey =
-            "eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiIsIng1dCI6IjREVjZzVkxIM0FtU1JTbUZqMk04Wm5wWHU3WSJ9.eyJuYW1laWQiOiIxNDc2ZjdlYS04MTZlLTRhOTctOWYzMy1iOTlhNWFjYjI2Y2UiLCJ0ZW5hbnRpZCI6ImQ2OThmMGRlLTczZjUtNGE4MS1hNGIxLWZmMzUzNTA5ZmFlYyIsImFwcGxpY2F0aW9uaWQiOiJBMDU2Q0E2Qi1BM0E4LTRBQzctQjMyNS05OTc2NjYzMDZFNTIiLCJQb2RJZCI6IlBvZDIiLCJpc3MiOiJodHRwczovL29hdXRoMi5za3kuYmxhY2tiYXVkLmNvbS8iLCJhdWQiOiJSRXgiLCJleHAiOjE0OTE1MTE0OTAsIm5iZiI6MTQ5MTUwNzg5MH0.IJLcUfbo-kSgh_mJ79DWpaqVST51a6WWlM1zHR6fi9QyJIW1oPraoTVg5h0Ms7F6EuVPqfXmf_mFvrw5owBMunYb5lJeP0s-ONX6Trd3lWiL-7Kywd8mWI-_C6lF70NQLCw8MB8IikoJfkwr5QOWpZKSO03w3nMRDWc40sjC602ztEtjnWtgtkU2zet6UeTYE5LjszLkTrInjMqsNBNq13nDtB__TEgHBMtGg8uiX8sbXaBxm_APYHfmK6Xp3QU_xOcPIS3PeW7oRB_CYiBSjYImHXstsfxKiwG17JFd-TdJB494BryzOAfNggheP8OR2gS-rZolv5ElIZgnmcm61A";
+            "eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiIsIng1dCI6IjREVjZzVkxIM0FtU1JTbUZqMk04Wm5wWHU3WSJ9.eyJuYW1laWQiOiIxNDc2ZjdlYS04MTZlLTRhOTctOWYzMy1iOTlhNWFjYjI2Y2UiLCJ0ZW5hbnRpZCI6ImQ2OThmMGRlLTczZjUtNGE4MS1hNGIxLWZmMzUzNTA5ZmFlYyIsImFwcGxpY2F0aW9uaWQiOiJBMDU2Q0E2Qi1BM0E4LTRBQzctQjMyNS05OTc2NjYzMDZFNTIiLCJQb2RJZCI6IlBvZDIiLCJpc3MiOiJodHRwczovL29hdXRoMi5za3kuYmxhY2tiYXVkLmNvbS8iLCJhdWQiOiJSRXgiLCJleHAiOjE0OTE1MDA3NDEsIm5iZiI6MTQ5MTQ5NzE0MX0.CN39_Xbl16lBhf0KJo6qaDY6EKbmbpujiAn9Z-WzqJRCjYXkVRoesneI-hGttCMn1mm-MUGDjuo5u7jhcXzP5nJ3RsVFjFY5fzOiRSeycRQ9JJruoTTrCoPAkJGzhErAPlTDCQu4ocyhq7Jn6G11P5I7h2cBjFaAj_ZSZqwjWj8KrVG8kt6a_7IpAwoUi4rc8l-Ka3vzQZS5y93VioN8ZfXQy77MElg2PzZIAs9AGrIMfy70Af2GgPTKxF_jGiGajeBKTws-Qy_IIlfJ1VPtiFlbjeGTuO2QkNjMXdzUkrvKnLJk8QmJIS1qQ1P5_YBzUl5ndSQBcYaYbEy1p2-pCQ";
     }
 }
